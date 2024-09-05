88235a0aac2e25ab83d2ec840b3be4a2f44a5101
abdalrhman <abdalrahmanabuyaqoub@gmail.com>
2024-09-05 19:18:41 +0300

    implementing core functionality (adding new task)

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log("hi");
+let newTaskInput = document.querySelector("#new-item");
+const addNewTask = document.querySelector(".add-button");
+const container = document.querySelector(".container");
+const header = document.querySelector(".header");
+const tasks = document.querySelector(".tasks");
+
+let newTaskText = "";
+
+newTaskInput.addEventListener("input", function (event) {
+  input(event.target.value);
+});
+
+addNewTask.addEventListener("click", function () {
+  addButton();
+});
+
+function addButton() {
+  console.log("button clicked");
+  createTask();
+}
+
+function createTask() {
+  if (newTaskText !== "") {
+    const task = document.createElement("div");
+    const checkbox = document.createElement("input");
+    const taskTextDiv = document.createElement("div");
+    const deleteButton = document.createElement("button");
+
+    deleteButton.innerHTML = "✕";
+    deleteButton.className = "delete-button";
+    task.appendChild(deleteButton);
+
+    taskTextDiv.innerHTML = newTaskText;
+    taskTextDiv.className = "task-text";
+    task.appendChild(taskTextDiv);
+
+    task.className = "task";
+    tasks.prepend(task);
+
+    checkbox.type = "checkbox";
+    checkbox.name = "task-checkbox";
+    checkbox.id = "task-checkbox";
+    task.appendChild(checkbox);
+
+    newTaskText = "";
+    newTaskInput.value = "";
+  }
+}
+
+function input(value) {
+  newTaskText = value;
+  console.log(newTaskText);
+}
